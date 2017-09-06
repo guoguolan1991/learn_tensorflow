58d357c240cca7ba5339be46a0bb3b917b1c85b8
guoguolan1991 <guoguolan1991@gmail.com>
2017-09-06 09:53:52 +0800

    change code style

--- classification/svm_word2vec.py
+++ classification/svm_word2vec.py
@@ -1,66 +1,58 @@
-#coding:utf-8
+# coding:utf-8
 import sys
+import gensim
+import numpy as np
+from sklearn.svm import SVC
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 VECTOR_DIR = '../data/wiki/wiki.cn.text.jian.model'
 
-MAX_SEQUENCE_LENGTH = 100
-EMBEDDING_DIM = 400
-TEST_SPLIT = 0.2
 
+class svm_word2vec(object):
+    def __init__(self):
+        self.EMBEDDING_DIM = 400
+        self.TEST_SPLIT = 0.2
+        self.w2v_model = gensim.models.Word2Vec.load(VECTOR_DIR)
 
-print '(1) load texts...'
-train_docs = open('../data/sougou/train_contents.txt').read().split('\n')
-train_labels = open('../data/sougou/train_labels.txt').read().split('\n')
-test_docs = open('../data/sougou/test_contents.txt').read().split('\n')
-test_labels = open('../data/sougou/test_labels.txt').read().split('\n')
+    def data_vector(self, datas):
+        result = []
+        for data in datas:
+            words = data.split(' ')
+            vector = np.zeros(self.EMBEDDING_DIM)
+            word_num = len(words)
+            for word in words:
+                if unicode(word) in self.w2v_model:
+                    vector += self.w2v_model[unicode(word)]
+            if word_num > 0:
+                vector = vector/word_num
+            result.append(vector)
+        return result
 
-print '(2) doc to var...'
-import gensim
-import numpy as np
-# w2v_model = gensim.models.KeyedVectors.load_word2vec_format(VECTOR_DIR, binary=True)
-w2v_model = gensim.models.Word2Vec.load(VECTOR_DIR)
-x_train = []
-x_test = []
-for train_doc in train_docs:
-    words = train_doc.split(' ')
-    vector = np.zeros(EMBEDDING_DIM)
-    word_num = 0
-    for word in words:
-        if unicode(word) in w2v_model:
-            vector += w2v_model[unicode(word)]
-            word_num += 1
-    if word_num > 0:
-        vector = vector/word_num
-    x_train.append(vector)
-for test_doc in test_docs:
-    words = test_doc.split(' ')
-    vector = np.zeros(EMBEDDING_DIM)
-    word_num = 0
-    for word in words:
-        if unicode(word) in w2v_model:
-            vector += w2v_model[unicode(word)]
-            word_num += 1
-    if word_num > 0:
-        vector = vector/word_num
-    x_test.append(vector)
-print 'train doc shape: '+str(len(x_train))+' , '+str(len(x_train[0]))
-print 'test doc shape: '+str(len(x_test))+' , '+str(len(x_test[0]))
-y_train = train_labels
-y_test = test_labels
+if __name__ == '__main__':
+    train_docs = open('../data/sougou/train_contents.txt').read().split('\n')
+    train_labels = open('../data/sougou/train_labels.txt').read().split('\n')
+    test_docs = open('../data/sougou/test_contents.txt').read().split('\n')
+    test_labels = open('../data/sougou/test_labels.txt').read().split('\n')
+    svm_word2vec = svm_word2vec()
+    x_train = svm_word2vec.data_vector(train_docs)
+    x_test = svm_word2vec.data_vector(test_docs)
 
-print '(3) SVM...'
-from sklearn.svm import SVC   
-svclf = SVC(kernel='linear')
-svclf.fit(x_train, y_train)
-preds = svclf.predict(x_test)
-num = 0
-preds = preds.tolist()
-for i, pred in enumerate(preds):
-    if int(pred) == int(y_test[i]):
-        num += 1
-print 'precision_score:' + str(float(num) / len(preds))
+    print 'train doc shape: '+str(len(x_train))+' , '+str(len(x_train[0]))
+    print 'test doc shape: '+str(len(x_test))+' , '+str(len(x_test[0]))
+    y_train = train_labels
+    y_test = test_labels
+
+    print 'SVM...'
+    svclf = SVC(kernel='linear')
+    svclf.fit(x_train, y_train)
+    preds = svclf.predict(x_test)
+    num = 0
+    preds = preds.tolist()
+    for i, pred in enumerate(preds):
+        if int(pred) == int(y_test[i]):
+            num += 1
+    print 'precision_score:' + str(float(num) / len(preds))
 
 
 
